7015c4ef3b9245e7a7a695617bd7322660b1d81e
Valeteg <160959269+Valeteg@users.noreply.github.com>
2024-03-01 15:24:28 -0500

    Update Fecha.py

--- Fecha.py
+++ Fecha.py
@@ -1,22 +1,27 @@
 class Fecha:
-    #aqui va el código
-    '''-------------------
-    #Atributos
-    -------------------'''
-
+    # aqui va el codigo
+    '''----------------------------------------------------------------
+    # Atributos
+    ----------------------------------------------------------------'''
+    
     dia=0
     mes=0
     anio=0
-    '''-----------------------------
-       #Metodos
-    ---------------------------'''
-    def ConsultarDia (self):
-        #aqui va el codigo
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    
+    def ConsultarDia(self):
+        return self.dia
+    
+    def ConsultarMes(self):
         return self.mes
-
-    def CondultarMes (self):
-        #aqui va el codigo
-        return self.mes
-    def ConsultarAnio (self):
-        #aqui va el codigo
+    
+    def ConsultarAnio(self):
         return self.anio
+    
+    def _init_(dia, mes ,anio):
+        self.dia = dia
+        self.mes = mes
+        self.anio = anio
