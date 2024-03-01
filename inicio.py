c9f1e56a2cb2001ac6b4d3274f5db64dfc84c3a4
Valeteg <160959269+Valeteg@users.noreply.github.com>
2024-03-01 15:38:02 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,1 +1,5 @@
+from Empleado import Empleado
 
+empleado1 = Empleado("Juan", "Perez", 1000000, 1)
+empleado2 = Empleado("Pedro", "Martines", 6000000, 1)
+empleado3 = Empleado("Maria", "Benitez", 6000000, 2)
