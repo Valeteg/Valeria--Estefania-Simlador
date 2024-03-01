be648a80d286884d1271a58a54e6b3c3b51ea178
Valeteg <160959269+Valeteg@users.noreply.github.com>
2024-03-01 15:22:17 -0500

    Update CuentaCorriente.py

--- CuentaCorriente.py
+++ CuentaCorriente.py
@@ -1,27 +1,24 @@
 class CuentaCorriente:
-    # aqui va la clase
-    '''------------------
-    #   Atributos
-    ------------------'''
-    saldo=0
-    interesMensual=0
-
-    """-----------------------------
-    Metodos
-    -----------------------------"""
-
-    def retirarSaldo(self):
-        # aqui va el codigo
-        retiro=0
-        retiroSaldo= self.saldo-retiro
-        return "Retiraste: "+self.retirasteSaldo
-
-    def consultarSaldo(self, saldo):
-        # aqui va la clase
+    # aqui va el codigo
+    '''----------------------------------------------------------------
+    # Atributos
+    ----------------------------------------------------------------'''
+    saldo = 0
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    def ConsultarSaldo(self):
         return self.saldo
-
-    def ConsultarInteres(cantidad):
-        #aqui va el codigo
-        interes = cantidad *0.0
-        return interes
-
+    
+    def ConsignarMonto(self, monto):
+        # Forma 1
+        self.saldo += monto
+        # Forma 2
+        # self.saldo = self.saldo + monto
+        
+    def RetirarMonto(self, monto):
+        # Forma 1
+        self.saldo -= monto
+        # Forma 2
+        # self.saldo = self.saldo - monto
