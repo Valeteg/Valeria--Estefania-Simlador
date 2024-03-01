ff346ba8198c038bf0338f782891f499b72b7ea9
Valeteg <160959269+Valeteg@users.noreply.github.com>
2024-03-01 15:21:36 -0500

    Update CuentaAhorros.py

--- CuentaAhorros.py
+++ CuentaAhorros.py
@@ -1,23 +1,28 @@
 class CuentaAhorros:
-    # aqui va la clase
-    '''------------------
-    #Atributos
-    ------------------'''
-    saldo=0
-    interesMensual=0
-
-    """----------------
-    Metodos
-    -------------------"""
-
-    def retirarSaldo(self):
-        # aqui va la clase
-        retiro=0
-        retiroSalario= self.salario-retiro
-        return "Retiraste: "+self.retirarSaldo
-
-    def ConsultarInteres(cantidad):
-        #aqui va el codigo
-        interes = cantidad *0.6
-        return interes
-
+    # aqui va el codigo
+    '''----------------------------------------------------------------
+    # Atributos
+    ----------------------------------------------------------------'''
+    saldo = 0
+    interesMensual = 0
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    def ConsultarSaldo(self):
+        return self.saldo
+    
+    def ConsultarInteresMensual(self):
+        return self.interesMensual
+    
+    def ConsignarMonto(self, monto):
+        # Forma 1
+        self.saldo += monto
+        # Forma 2
+        # self.saldo = self.saldo + monto
+        
+    def RetirarMonto(self, monto):
+        # Forma 1
+        self.saldo -= monto
+        # Forma 2
+        # self.saldo = self.saldo - monto
